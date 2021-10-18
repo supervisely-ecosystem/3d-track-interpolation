1b66de8017d95a30098718582efa58c75d56b328
kashkarov <kashkarov@dedicorp.ru>
2021-10-18 18:26:11 +0300

    minor: update test example

--- src/test.py
+++ src/test.py
@@ -4,7 +4,8 @@ import sly_globals as g
 
 def api_test():
     data = {"figures_ids": [56655589, 56655590, 56655621, 56655596],
-            "dataset_id": 33090}
+            "dataset_id": 33090,
+            "track_id": "hello_track!"}
     r = g.api.task.send_request(g.task_id, "interpolate_figures_ids", data=data)
 
     print(json.dumps(r, indent=4))
